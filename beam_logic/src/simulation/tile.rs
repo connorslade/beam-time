connorslade/beam-time

commit 69932a6f512f64b368573733b72578222d327a7a

Allow checking powered status of non detector tiles in level checkers

beam_logic/src/simulation/tile.rs
@@ -53,9 +53,12 @@ impl BeamTile {
     pub fn is_powered(&self) -> bool {
         match self {
             Self::Emitter { active: true, .. } | Self::Beam { .. } | Self::CrossBeam { .. } => true,
+            Self::Detector { powered }
+            | Self::Galvo { powered, .. }
+            | Self::Delay { powered, .. }
+            | Self::Wall { powered } => powered.any(),
             Self::Mirror { powered, .. } => powered[0].is_some() || powered[1].is_some(),
             Self::Splitter { powered, .. } => powered.is_some(),
-            Self::Delay { last_powered, .. } => last_powered.any(),
             _ => false,
         }
     }

beam_logic/src/simulation/level_state.rs
@@ -118,11 +118,7 @@ impl LevelState {
             .iter()
             .map(|pos| {
                 let pos = pos.into_pos();
-                let BeamTile::Detector { powered } = board.get(pos) else {
-                    return false;
-                };
-
-                powered.any()
+                board.get(pos).is_powered()
             })
             .collect()
     }

beam_time/src/game/render/board.rs
@@ -56,7 +56,7 @@ impl Board {
             self.transient.history.pop(&mut self.tiles);
         }
 
-        const MESSAGE: &str = "This is a test of the new label system I am considering adding to Beam time to improve the user experance of sandbox worlds.";
+        const MESSAGE: &str = "This is a test of the new label system I am considering adding to Beam time to improve the user experience of sandbox worlds.";
         for (pos, title, note) in [(Vector2::new(10.0, 10.0), "Little Note", MESSAGE)] {
             let mut pos = shared.world_to_screen_space(ctx, pos);
             let marker_pos = pos - Vector2::y() * 4.0 * 2.0 * ctx.scale_factor;

engine/src/input.rs
@@ -103,7 +103,7 @@ impl InputManager {
             WindowEvent::MouseWheel { delta, .. } => {
                 self.scroll_delta += match delta {
                     MouseScrollDelta::LineDelta(_, y) => *y,
-                    MouseScrollDelta::PixelDelta(pos) => pos.y as f32,
+                    MouseScrollDelta::PixelDelta(pos) => pos.y as f32 / 10.0,
                 };
             }
             WindowEvent::KeyboardInput { event, .. } if !event.repeat => {

commit aaceb88e9529f15e37630441741971bf65ab0f2d

Fix delay gate shortning pulses Bug was introduced in 69932a6f512f64b368573733b72578222d327a7a

beam_logic/src/simulation/tile.rs
@@ -53,10 +53,10 @@ impl BeamTile {
     pub fn is_powered(&self) -> bool {
         match self {
             Self::Emitter { active: true, .. } | Self::Beam { .. } | Self::CrossBeam { .. } => true,
-            Self::Detector { powered }
-            | Self::Galvo { powered, .. }
-            | Self::Delay { powered, .. }
-            | Self::Wall { powered } => powered.any(),
+            Self::Detector { powered } | Self::Galvo { powered, .. } | Self::Wall { powered } => {
+                powered.any()
+            }
+            Self::Delay { last_powered, .. } => last_powered.any(),
             Self::Mirror { powered, .. } => powered[0].is_some() || powered[1].is_some(),
             Self::Splitter { powered, .. } => powered.is_some(),
             _ => false,