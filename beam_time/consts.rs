connorslade/beam-time

commit 6fa60839775a96d6b71202872b4dccdfa5566b21

Package bin in workflow

beam_time/game/selection.rs
@@ -121,13 +121,9 @@ impl Board {
             let mut old = Vec::new();
 
             for pos in this.selection.iter() {
-                let mut tile = self.tiles.get(*pos);
-                if cut {
-                    old.push((*pos, tile));
-                    self.tiles.remove(*pos);
-                } else {
-                    tile = tile.generic();
-                }
+                let tile = self.tiles.get(*pos);
+                old.push((*pos, tile));
+                self.tiles.remove(*pos);
 
                 if !tile.is_empty() {
                     list.push((*pos, tile));
@@ -149,10 +145,11 @@ impl Board {
 
         if ctrl
             && paste
-            && let Some(item) = state.clipboard.clone()
+            && let Some(item) = &state.clipboard
         {
             *sim = None;
-            self.transient.holding = Holding::Paste(item);
+            self.transient.holding =
+                Holding::Paste(item.iter().map(|(p, x)| (*p, x.generic())).collect());
         }
     }
 

.github/workflows/publish.yml
@@ -6,11 +6,17 @@ jobs:
     strategy:
       fail-fast: false
       matrix:
-        os: [ubuntu-latest, macos-14-large, windows-latest]
+        os: [ubuntu-latest, macos-latest, windows-latest]
     runs-on: ${{ matrix.os }}
 
     steps:
-      - uses: actions/checkout@v4
+      - uses: actions/checkout@v5
+      - uses: actions/checkout@v5
+        with:
+          repository: "rlabrecque/SteamworksSDK"
+          sparse-checkout: redistributable_bin
+          path: steamworks-sdk
+
       - uses: hecrj/setup-rust-action@v2
         with:
           rust-version: nightly
@@ -24,11 +30,27 @@ jobs:
         env:
           BEAMTIME_HMAC: ${{ secrets.BEAMTIME_HMAC }}
 
-      - name: Upload Build Artifacts
+      - name: Package for Steam
+        run: |
+          mkdir dist
+          if [ "$RUNNER_OS" == "Linux" ]; then
+            cp target/release/beam_time dist
+            cp steamworks-sdk/redistributable_bin/linux64/libsteam_api.so dist
+          elif [ "$RUNNER_OS" == "Windows" ]; then
+            cp target\release\beam_time.exe dist
+            cp steamworks-sdk\redistributable_bin\win64\steam_api64.dll dist
+          elif [ "$RUNNER_OS" == "macOS" ]; then
+            cp -r beam_time/dist/Beam\ Time.app dist
+            cp target/release/beam_time dist/Beam\ Time.app/MacOS
+            cp steamworks-sdk/redistributable_bin/osx/libsteam_api.dylib dist/Beam\ Time.app/MacOS
+          else
+                echo "$RUNNER_OS not supported"
+                exit 1
+          fi
+        shell: bash
+
+      - name: Upload Zipped Artifacts
         uses: actions/upload-artifact@v4
         with:
-          name: beam_time-${{ matrix.os }}
-          path: |
-            target/release/beam_time*
-            !target/release/*.d
-            !target/release/*.pdb
+          name: beam_time-${{ runner.os }}
+          path: dist/*

beam_time/game/render/board.rs
@@ -35,10 +35,10 @@ impl Board {
         let tile_counts = pancam.tile_counts(ctx.size());
         let frame = state.frame();
 
+        self.update_selection(ctx, state, pancam, sim);
         self.transient
             .holding
             .render(ctx, pancam, self.transient.level);
-        self.update_selection(ctx, state, pancam, sim);
         self.render_notes(ctx, state, pancam);
 
         if sim.is_none() && ctx.input.key_down(CTRL) && ctx.input.key_pressed(KeyCode::KeyZ) {

commit 6c41799dccd4efc482fbe8e9b925f4dd371ecb53

Restore correct cut/copy behavior
Error introduced in 6fa6083

beam_time/consts.rs
@@ -101,6 +101,9 @@ pub mod keybind {
     pub const SPEED_DOWN: KeyCode = KeyCode::Minus;
     pub const SPEED_RESET: KeyCode = KeyCode::Digit0;
 
+    pub const PREV_CASE: KeyCode = KeyCode::ArrowLeft;
+    pub const NEXT_CASE: KeyCode = KeyCode::ArrowRight;
+
     // pancam movement
     pub const UP: KeyCode = KeyCode::KeyW;
     pub const DOWN: KeyCode = KeyCode::KeyS;

beam_time/game/holding.rs
@@ -21,7 +21,7 @@ use super::pancam::Pancam;
 
 pub type ClipboardItem = Vec<(Vector2<i32>, Tile)>;
 
-#[derive(Default, Clone, PartialEq, Eq)]
+#[derive(Default, Debug, Clone, PartialEq, Eq)]
 pub enum Holding {
     #[default]
     None,
@@ -79,9 +79,11 @@ impl Holding {
                             }
                         }
                     },
-                    keybind::FLIP_V => for (pos, tile) in tiles.iter_mut() {
-                        *pos = Vector2::new(pos.x, -pos.y);
-                        *tile = tile.flip_vertical();
+                    keybind::FLIP_V => if !ctx.input.key_down(keybind::CTRL) {
+                        for (pos, tile) in tiles.iter_mut() {
+                            *pos = Vector2::new(pos.x, -pos.y);
+                            *tile = tile.flip_vertical();
+                        }
                     },
                     keybind::FLIP_H => for (pos, tile) in tiles.iter_mut() {
                         *pos = Vector2::new(-pos.x, pos.y);

beam_time/game/selection.rs
@@ -118,13 +118,15 @@ impl Board {
             let mut old = Vec::new();
 
             for pos in this.selection.iter() {
-                let tile = self.tiles.get(*pos);
-                old.push((*pos, tile));
-                self.tiles.remove(*pos);
-
-                if !tile.is_empty() {
-                    list.push((*pos, tile));
+                let mut tile = self.tiles.get(*pos);
+                if cut {
+                    old.push((*pos, tile));
+                    self.tiles.remove(*pos);
+                } else {
+                    tile = tile.generic();
                 }
+
+                (!tile.is_empty()).then(|| list.push((*pos, tile)));
             }
 
             cut.then(|| self.transient.history.track_many(old));

beam_time/util/mod.rs
@@ -21,7 +21,7 @@ pub macro key_events(
 pub fn enable_console() {
     #[cfg(target_os = "windows")]
     unsafe {
-        use windows::Win32::System::Console::{AttachConsole, FreeConsole, ATTACH_PARENT_PROCESS};
+        use windows::Win32::System::Console::{ATTACH_PARENT_PROCESS, AttachConsole, FreeConsole};
         let _ = FreeConsole();
         let _ = AttachConsole(ATTACH_PARENT_PROCESS);
     }

beam_time/ui/level_panel/test_case.rs
@@ -2,8 +2,8 @@ use parking_lot::MutexGuard;
 
 use crate::{
     assets::{
-        BIG_RIGHT_ARROW, DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW, TILE_DETECTOR,
-        TILE_EMITTER_DOWN, UNDEAD_FONT,
+        BIG_RIGHT_ARROW, DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW, TILE_DETECTOR, TILE_EMITTER_DOWN,
+        UNDEAD_FONT,
     },
     consts::{keybind, spacing::MARGIN},
     ui::{components::manual_button::ManualButton, level_panel::horizontal_rule, misc::tile_label},
@@ -35,8 +35,8 @@ impl LevelPanel {
         sim: &MutexGuard<InnerAsyncSimulationState>,
     ) {
         let test_count = level.tests.visible_count();
-        (ctx.input.key_pressed(keybind::LEFT) && self.case > 0).then(|| self.case -= 1);
-        (ctx.input.key_pressed(keybind::RIGHT) && self.case + 1 < test_count)
+        (ctx.input.key_pressed(keybind::PREV_CASE) && self.case > 0).then(|| self.case -= 1);
+        (ctx.input.key_pressed(keybind::NEXT_CASE) && self.case + 1 < test_count)
             .then(|| self.case += 1);
 
         let sim_level = sim.beam.as_ref().and_then(|x| x.level.as_ref());